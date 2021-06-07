c806172393cc0cb9aed47a587cd14ab2d34efc7e
gefranco <933@holbertonschool.com>
2021-06-07 23:36:30 +0000

    [testing]

--- naive_malloc.c
+++ naive_malloc.c
@@ -11,9 +11,11 @@ void *naive_malloc(size_t size)
 	static int x;
 	static void *brk_base;
 
+	void *h;
 	if (size == 0)
 		return (NULL);
 
+	
 	size += sizeof(size_t);
 	size = align(size);
 
@@ -22,17 +24,22 @@ void *naive_malloc(size_t size)
 		x = 0;
 		brk_base = sbrk(sysconf(_SC_PAGESIZE));
 		if (brk_base == (void *)-1)
-		return (NULL);
+			return (NULL);
 	}
 
 	/*for (i = 0; i < x; i += 1)*/
+h = brk_base;
+	if (x > 0)
 	{
+        	/*h = ((void *)((char *)brk_base));*/
 		brk_base = ((void *)((char *)brk_base + size));
 	}
 
 	x += 1;
-	memcpy(brk_base, &size, sizeof(size));
-	brk_base = ((char *)brk_base) + sizeof(size_t);
+	/*h = brk_base;
+	h = ((void *)((char *)brk_base - size + sizeof(size_t)));*/
+	memcpy(h, &size, sizeof(size));
+	brk_base = ((char *)h) + sizeof(size_t);
 	return (brk_base);
 }
 
